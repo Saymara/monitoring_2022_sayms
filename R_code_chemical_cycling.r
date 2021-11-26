6dd45dbc7a90af8767daf8213905e9841061f8de
Saymara <92572718+Saymara@users.noreply.github.com>
2021-11-26 10:51:22 +0100

    Create R_code_chemical_cycling.r

---
+++ R_code_chemical_cycling.r
@@ -0,0 +1,97 @@
+# R coding for chemical  cycling studying
+# time series of NO2 change in Europe during the pandemic
+
+library(raster)
+
+# set the working directory
+
+setwd("C:/lab/en/")  
+
+en01 <- raster("EN_0001.png")
+# what's the range?
+en01
+
+cl <- colorRampPalette(c('red','orange','yellow'))(100) 
+
+# plot the the NO2 values of January 2020 by the cl palette
+plot(en01, col=cl)
+
+# Exercise: import the end of March NO2 and plot it
+
+en13 <- raster("EN_0013.png")
+plot(en13, col=cl)
+
+par(mfrow=c(2,1))
+plot(en01,col=cl)
+plot(en13,col=cl)
+
+# import all the images
+EN01 <- raster("EN_0001.png")
+EN02 <- raster("EN_0002.png")
+EN03 <- raster("EN_0003.png")
+EN04 <- raster("EN_0004.png")
+EN05 <- raster("EN_0005.png")
+EN06 <- raster("EN_0006.png")
+EN07 <- raster("EN_0007.png")
+EN08 <- raster("EN_0008.png")
+EN09 <- raster("EN_0009.png")
+EN10 <- raster("EN_0010.png")
+EN11 <- raster("EN_0011.png")
+EN12 <- raster("EN_0012.png")
+EN13 <- raster("EN_0013.png")
+
+
+ # plot all the data together
+ par(mfrow=c(4,4)
+ [10:26 AM] Duccio Rocchini
+# plot all the data together
+
+par(mfrow=c(4,4))
+
+plot(EN01, col=cl)
+
+plot(EN02, col=cl)
+
+plot(EN03, col=cl)
+
+plot(EN04, col=cl)
+
+plot(EN05, col=cl)
+
+plot(EN06, col=cl)
+
+plot(EN07, col=cl)
+
+plot(EN08, col=cl)
+
+plot(EN09, col=cl)
+
+plot(EN10, col=cl)
+
+plot(EN11, col=cl)
+
+plot(EN12, col=cl)
+
+plot(EN13, col=cl)
+
+
+ 
+# plot the stack together
+
+EN <- stack(EN01, EN02, EN03, EN04, EN05, EN06, EN07, EN08, EN09, EN10, EN11, EN12, EN13)
+
+# plot the stack all together
+plot(EN,col=cl)
+
+# plot only the first image of the stack
+
+EN
+plot(EN$EN_0001, col=cl)
+
+# RGB space 
+
+plotRGB(EN, r=1 ,g=7,  b=13, stretch="lin") # in the r band are represented all the areas with highest amounts of NO2 in January and so on
+
+
+# pairs
+pairs(EN)
