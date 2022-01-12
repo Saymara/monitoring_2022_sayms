476923889a2b2edfbeefb71f54a5bd1b3bc2828a
Saymara <92572718+Saymara@users.noreply.github.com>
2022-01-12 10:12:56 +0100

    Update R_code_sdm.r

--- R_code_sdm.r
+++ R_code_sdm.r
@@ -27,12 +27,12 @@ species[species$Occurrence == 1,]
 presences <- species[species$Occurrence == 1,]
 absences <- species[species$Occurrence == 0,]
 
- # plot! 
+# plot! 
  
- plot(species, pch=19)
- plot(presences, pch=19, col="blue")
+plot(species, pch=19)
+lot(presences, pch=19, col="blue")
  
- # plot with presences and absences
+# plot with presences and absences
 
 plot(presences, pch=19, col="blue")
 points(absences, pch=19, col="red") # this function allows us to represent both presences and absences together in the plot
@@ -42,12 +42,14 @@ points(absences, pch=19, col="red") # this function allows us to represent both 
 
 path <- system.file("external", package="sdm")
 
+# list the predictors
+
 lst <- list.files(path, pattern='asc', full.names=T)
 lst
 
  # you can use the lapply function with the raster function 
  
- preds <- stack(lst)
+ preds <- stack(lst)   # the stack function concatenates multiple vectors into a single vector along with a factor indicating where each observation originated.
  preds
  
  # plot preds
@@ -70,6 +72,8 @@ points(presences, pch=19)
 
 # day 2 (11 - 1 - 2022)
 
+# model
+
 setwd("C:/lab/")
 
 source("R_code_source_sdm.r")   # this function (source) allows us to import/ recall the data from our working directory (lab folder) into R
@@ -79,15 +83,21 @@ preds
 
 # these are the predictors: elevation, precipitation, temperature, vegetation
 
-# Let's explain to the model what are the traning and and predictors data
-# here we are basically asking the function to show us our  object (species) and the predictors (variables that influence the probability of the variable to be present or absent in a certain area in some sort of classification data ; (Creates a sdmdata objects that holds species (single or multiple) and explanatory variates. In addition, more information such as spatial coordinates, time, grouping variables, and metadata (e.g., author, date, reference, etc.) can be included);
+# Let's explain to the model what are the traning and and predictors data (set the data for the sdm)
+# here we are basically asking the function (datasdm) to show us our  object (species) and the predictors (variables that influence the probability of the variable to be present or absent in a certain area in some sort of classification data ; (Creates a sdmdata objects that holds species (single or multiple) and explanatory variates. In addition, more information such as spatial coordinates, time, grouping variables, and metadata (e.g., author, date, reference, etc.) can be included);
+
 datasdm <- sdmData(train=species, predictors=preds) 
 datasdm
 
+# model
+
 m1 <- sdm(Occurrence ~ elevation + precipitation + temperature + vegetation, data=datasdm, methods = "glm")
 
-#  prediction: map probability
+#  prediction: map probability ( the raster output layer)
+
 p1 <- predict(m1, newdata=preds)
+
+# plot the output
 
 plot(p1, col=cl)
 points(presences, pch=19)
@@ -97,7 +107,13 @@ s1 <- stack(preds, p1)
 plot(p1, col=cl)
 
 
+# Do you want to change names in the plot of the stack?
+# choose a vector of names for the stack, looking at the previous graph, which are:
+
 names(s1) <- c('Elevation', 'Precipitation', 'Temperature', 'Vegetation', 'Probability')
+
+# and then replot!:
+
 plot(s1, col=cl)
 
 
