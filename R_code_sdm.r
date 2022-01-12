f1a642cc26b75c29fd7a60bd2f285d61498cc612
Saymara <92572718+Saymara@users.noreply.github.com>
2022-01-12 09:53:39 +0100

    Update R_code_sdm.r

--- R_code_sdm.r
+++ R_code_sdm.r
@@ -68,19 +68,41 @@ points(presences, pch=19)
 
 ---------------------- \\ --------------- || ------------------- // ------------------------ || --------------------- \\ --------------- || ------------------- // ------------------------
 
-# day 2 (11 - 1 - 2021)
+# day 2 (11 - 1 - 2022)
 
 setwd("C:/lab/")
-source("R_code_source_sdm.r")   # this function allows us to import data from our working directory (lab folder) into R
+
+source("R_code_source_sdm.r")   # this function (source) allows us to import/ recall the data from our working directory (lab folder) into R
 
 # in the theoretical slides of SDMs we sghuld use individuals
 preds
 
 # these are the predictors: elevation, precipitation, temperature, vegetation
 
-# Let's explain to the model whar are the traning and and predictors data
-datasdm <- sdmData(train=species, predictors=preds)
+# Let's explain to the model what are the traning and and predictors data
+# here we are basically asking the function to show us our  object (species) and the predictors (variables that influence the probability of the variable to be present or absent in a certain area in some sort of classification data ; (Creates a sdmdata objects that holds species (single or multiple) and explanatory variates. In addition, more information such as spatial coordinates, time, grouping variables, and metadata (e.g., author, date, reference, etc.) can be included);
+datasdm <- sdmData(train=species, predictors=preds) 
 datasdm
 
-m1 <- sdm(Occurrence~temperature+elevation+precipitation+vegetation, data=datasdm, methods="glms")
+m1 <- sdm(Occurrence~temperature+elevation+precipitation+vegetation, data=datasdm, method="glms")
+
+#  prediction: map probability
+p1 <- predict(m1, newdata=preds)
+
+plot(p1, col=cl)
+points(presences, pch=19)
+
+# stack everything together
+s1 <- stack(preds, p1)
+plot(p1, col=cl)
+
+
+names(s1) <- c('Elevation', 'Precipitation', 'Temperature', 'Vegetation', 'Probability')
+plot(s1, col=cl)
+
+
+
+---------------------- \\ --------------- || ------------------- // ------------------------ || --------------------- \\ --------------- || ------------------- // ------------------------
+
+
 
